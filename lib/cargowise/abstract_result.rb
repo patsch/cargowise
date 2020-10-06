45bfd3902252e49f6be8decd0363c4abecd7c9e9
Patrick Dockhorn <patrick@apptastic.com.au>
2020-10-06 19:14:54 +1100

    stripping weird unicode characters from within cargowise

--- lib/cargowise/abstract_result.rb
+++ lib/cargowise/abstract_result.rb
@@ -32,12 +32,20 @@ module Cargowise
 
     def text_value(path)
       path = path.gsub("/","/tns:")
-      node.xpath("#{path}/text()", "tns" => Cargowise::DEFAULT_NS).to_s
+      deunicode(node.xpath("#{path}/text()", "tns" => Cargowise::DEFAULT_NS).to_s)
+    end
+
+    def deunicode(s)
+      ret = s
+      if !s.blank?
+        ret = s.gsub("_x0020_"," ").gsub("_x002F_","/").gsub("_x002f_","/").gsub("_x00b4_","'").gsub("_x00B4_","'").gsub("_x002B_","+").gsub("_x002b_","+")
+      end
+      ret
     end
 
     def attribute_value(path)
       path = path.gsub(/\/([^@])/u,'/tns:\1')
-      node.xpath(path, "tns" => Cargowise::DEFAULT_NS).to_s
+      deunicode(node.xpath(path, "tns" => Cargowise::DEFAULT_NS).to_s)
     end
 
     def time_value(path)
